3dc25ed6d31eb2033b79863805c0360f7b6d9f81
Han Zhang <michaelzhhan1990@gmail.com>
2018-08-14 18:36:51 +0800

    check number of <10
    check number of <10

--- script/feedid2url.py
+++ script/feedid2url.py
@@ -2,6 +2,8 @@ import random
 def feedid2url(file_in,file_out,top_k):
     count_cluster=0
     container=[]
+    num_larger_10=0
+    num_smaller_10=0
     with open(file_out, 'w', encoding='utf-8') as f_out:
         with open(file_in, encoding='utf-8') as f_in:
             text='1'
@@ -10,11 +12,13 @@ def feedid2url(file_in,file_out,top_k):
                 print(text)
                 if text.startswith('Cluster'):
                     if len(container) >10:
-                        for ite in random.sample(container,10):
-                            f_out.write(ite)
+                        num_larger_10 += 1
+                        #for ite in random.sample(container,10):
+                        #    f_out.write(ite)
                     else:
-                        for ite in container:
-                            f_out.write(ite)
+                        num_smaller_10+=1
+                        #for ite in container:
+                        #    f_out.write(ite)
                     f_out.write('\n')
                     count_cluster += 1
                     if count_cluster>=10000:
@@ -31,15 +35,20 @@ def feedid2url(file_in,file_out,top_k):
                     container.append('https://h5.qzone.qq.com/weishi/feed?_proxy=1&_wv=1&id='+text)
                     #f_out.write('https://h5.qzone.qq.com/weishi/feed?_proxy=1&_wv=1&id='+text)
         if len(container) > 10:
-            for ite in random.sample(container, 10):
-                f_out.write(ite)
+            #for ite in random.sample(container, 10):
+                #f_out.write(ite)
+            num_larger_10+=1
         else:
-            for ite in container:
-                f_out.write(ite)
+            num_smaller_10+=1
+            #for ite in container:
+                #f_out.write(ite)
+
+    print(num_larger_10)
+    print(num_smaller_10)
 
 
-file_in='C:\\Users\\mycohzhang\\source\\text_processing\\data\\5000_clusters_1000_iters_top_terms.txt'
-file_out='C:\\Users\\mycohzhang\\source\\text_processing\\data\\annotation\\5000_clusters_1000_iters_top_terms.txt'
+file_in='C:\\Users\\mycohzhang\\source\\text_clustering\\data\\5000_clusters_1000_iters_top_terms_real.txt'
+file_out='C:\\Users\\mycohzhang\\source\\text_clustering\\data\\annotation\\5000_clusters_1000_iters_top_terms_wrong.txt'
 top_k=10
 feedid2url(file_in,file_out,5000)
 
