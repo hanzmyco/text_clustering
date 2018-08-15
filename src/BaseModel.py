f8253b7c2a3214d744ad232bcd48e9f87005dbe2
Han Zhang <michaelzhhan1990@gmail.com>
2018-08-15 11:35:04 +0800

    fix bug for vector serilization
    fix bug for vector serilization

--- src/BaseModel.py
+++ src/BaseModel.py
@@ -2,6 +2,7 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import Normalizer
 from sklearn.pipeline import make_pipeline
 import config
+from sklearn.externals import joblib
 
 class BaseModel(object):
     def __init__(self,model=None):
@@ -15,3 +16,21 @@ class BaseModel(object):
             normalizer = Normalizer(copy=False)
             lsa = make_pipeline(svd, normalizer)
             self.feature = lsa.fit_transform(self.feature)
+
+    def serilize_model(self):
+        joblib.dump(self.vectorizer,config.vectorizer_file_name,compress=True)
+
+    def de_serilize_model(self):
+        self.vectorizer = joblib.load(config.vectorizer_file_name)
+
+    def transform(self,data_in):
+        feature = self.vectorizer.transform(data_in)
+
+        if config.n_components:
+            svd = TruncatedSVD(config.n_components)
+            normalizer = Normalizer(copy=False)
+            lsa = make_pipeline(svd, normalizer)
+            return lsa.fit_transform(feature)
+        else:
+            return feature
+
--- src/short_text_clustering.py
+++ src/short_text_clustering.py
@@ -3,21 +3,26 @@ import text_processing.src.read_json as read_json
 import Vectorizer
 import KMeans
 import Algorithm
+import BaseModel
 
 def main():
-    if config.model_name =='Counter':
-        model = Vectorizer.CounterVector(config.model_name)
-    elif config.model_name == 'TfIdf':
-        model = Vectorizer.TfIdfVector(config.model_name)
-        print('finish initilizing model')
 
     data_in=[]
     feed_id=[]
+    print('start reading data')
     read_json.read_json(config.path_in,data_in,config.stop_word_path,feed_id,config.data_lines)
     print('finish reading data')
-    model.feature_transform(data_in)
 
     if config.mode =='Training':
+        if config.model_name == 'Counter':
+            model = Vectorizer.CounterVector(config.model_name)
+        elif config.model_name == 'TfIdf':
+            model = Vectorizer.TfIdfVector(config.model_name)
+            print('finish initilizing model')
+
+        model.feature_transform(data_in)
+        model.serilize_model()
+
         if config.algo_name =='KMeans':
             algo_instance = KMeans.KMeansClustering(config.algo_name)
             print('start training model')
@@ -27,12 +32,19 @@ def main():
             algo_instance.output_cluster_info(data_in,model,feed_id)
 
     else:
-        print('start inference')
+        print('loading vectorizer')
+        model=BaseModel.BaseModel(config.model_name)
+        model.de_serilize_model()
+        print('finish loading vector')
+
+
         if config.algo_name =='KMeans':
-            algo_instance = KMeans.KMeansClustering(config.algo_name)
+            algo_instance = Algorithm.Base_Algorithm(config.algo_name)
             algo_instance.de_serilize_model()
             print('finish desirialization')
-            labels=algo_instance.predict(model.feature)
+            features=model.transform(data_in)
+
+            labels=algo_instance.predict(features)
             print(labels)
             #algo_instance.get_centroids()
             #algo_instance.output_cluster_info(data_in, model, feed_id)
